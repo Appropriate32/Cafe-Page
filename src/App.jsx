2ad1c394c0073c23df999cdc348c00d20ab334b1
Appropriate32 <frostghost32@gmail.com>
2026-06-17 21:56:53 +0500

    Installed shadcn, and removed daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div className="min-h-screen bg-base-200 flex items-center justify-center">
+    <div className="min-h-screen bg-base-200 bg-gray-700 flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-5xl font-bold mb-4">MERN FRONTEND Template</h1>
         <p className="mb-6">Vite + React + Tailwind v4 + DaisyUI</p>
         <button className="btn btn-primary">Ready to Code</button>
+        <h1 className="text-6xl font-bold mt-4 text-red-500">I like femboys</h1>
       </div>
     </div>
   );
